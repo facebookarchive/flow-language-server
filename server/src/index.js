d3c8f5c14f4ad4d5b7805276bc697341da05a938
William Binns-Smith <wbinnssmith@gmail.com>
2017-04-18 15:44:34 -0700

    Add vscode extension and simple server

---
+++ server/src/index.js
@@ -0,0 +1,25 @@
+// @flow
+
+import 'regenerator-runtime/runtime';
+
+import {
+  IPCMessageReader,
+  IPCMessageWriter,
+  createConnection,
+} from 'vscode-languageserver';
+
+process.on('uncaughtException', e => console.error('uncaughtException', e));
+process.on('unhandledRejection', e => console.error('unhandledRejection', e));
+
+const connection = createConnection(
+  new IPCMessageReader(process),
+  new IPCMessageWriter(process),
+);
+
+connection.onInitialize(params => {
+  return {
+    capabilities: {},
+  };
+});
+
+connection.listen();
