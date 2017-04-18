3bdb5473ae8772e55d69a30e672a3d1cdee3df80
William Binns-Smith <wbinnssmith@gmail.com>
2017-04-18 16:12:32 -0700

    Add diagnostics

--- server/src/index.js
+++ server/src/index.js
@@ -6,19 +6,46 @@ import {
   IPCMessageReader,
   IPCMessageWriter,
   createConnection,
+  TextDocuments,
 } from 'vscode-languageserver';
 
-process.on('uncaughtException', e => console.error('uncaughtException', e));
-process.on('unhandledRejection', e => console.error('unhandledRejection', e));
+import Diagnostics from './Diagnostics';
+import {getLogger} from './pkg/nuclide-logging/lib/main';
+
+const logger = getLogger();
+
+process.on('uncaughtException', e => logger.error('uncaughtException', e));
+process.on('unhandledRejection', e => logger.error('unhandledRejection', e));
 
 const connection = createConnection(
   new IPCMessageReader(process),
   new IPCMessageWriter(process),
 );
 
+const documents = new TextDocuments();
+documents.listen(connection);
+
 connection.onInitialize(params => {
+  // TODO: Explicitly pass this through to FlowHelpers.js
+  global.workspacePath = params.rootPath;
+
+  const diagnostics = new Diagnostics(connection, documents);
+  connection.onDidChangeConfiguration(({settings}) => {
+    logger.info('config changed');
+    documents.all().forEach(doc => diagnostics.validate(doc));
+  });
+
+  documents.onDidChangeContent(({document}) => {
+    logger.info('content changed');
+    diagnostics.validate(document);
+  });
+
+  logger.info('Flow language server started');
+
   return {
-    capabilities: {},
+    capabilities: {
+      textDocumentSync: documents.syncKind,
+    },
   };
 });
 
