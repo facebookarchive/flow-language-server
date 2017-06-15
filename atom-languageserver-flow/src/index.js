90c78b730dbec1c1be2fea0f4caff8034b0bed6d
hansonw <hansonw@fb.com>
2017-06-14 21:45:19 -0700

    Busy signal support via special logging messages

--- atom-languageserver-flow/src/index.js
+++ atom-languageserver-flow/src/index.js
@@ -1,12 +1,29 @@
 // @flow
 
+// $FlowFixMe: coming soon
+import type {BusySignalService} from 'atom-ide-ui';
+
+import {Disposable} from 'atom';
 import {AutoLanguageClient} from 'atom-languageclient';
 import {spawn} from 'child_process';
 import * as path from 'path';
 
 const SERVER_HOME = path.join(__dirname, '..', '..', 'server', 'lib', 'bin');
 
+// As defined in the LSP.
+const LOG_MESSAGE_INFO = 3;
+
+// Keep these in sync with the server side.
+const STATUS_PREFIX = 'Flow status: ';
+const BUSY_STATUS = Object.freeze({
+  busy: 'busy',
+  init: 'initializing',
+});
+
 class FlowLanguageServer extends AutoLanguageClient {
+  // TODO: technically we need one per connection.
+  _lastBusyMessage: ?IDisposable;
+
   getConnectionType() {
     return 'ipc';
   }
@@ -32,7 +49,42 @@ class FlowLanguageServer extends AutoLanguageClient {
       },
     ));
   }
+
+  preInitialization(connection) {
+    connection.onLogMessage(e => {
+      if (
+        this._busySignalService != null &&
+        e.type === LOG_MESSAGE_INFO &&
+        e.message.startsWith(STATUS_PREFIX)
+      ) {
+        if (this._lastBusyMessage) {
+          this._lastBusyMessage.dispose();
+        }
+        const status = e.message.substr(STATUS_PREFIX.length);
+        const statusText = BUSY_STATUS[status];
+        if (statusText) {
+          this._lastBusyMessage = this._busySignalService.reportBusy(
+            `Flow server is ${statusText}...`,
+          );
+        }
+      }
+    });
+    // TODO: needs a public API
+    connection._rpc.onClose(() => {
+      if (this._lastBusyMessage) {
+        this._lastBusyMessage.dispose();
+      }
+    });
+  }
+
+  consumeBusySignal(busySignalService: BusySignalService) {
+    this._busySignalService = busySignalService;
+    this._disposable.add(busySignalService);
+    return new Disposable(() => {
+      this._busySignalService = null;
+      this._disposable.remove(busySignalService);
+    });
+  }
 }
 
 module.exports = new FlowLanguageServer();
-
