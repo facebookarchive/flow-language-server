be110814571d2fb30e27ca53b28dbd565b82300a
Will Binns-Smith <wbinnssmith@fb.com>
2018-02-27 17:47:19 -0800

    Update lint deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,12 @@ const rulesDirPlugin = require('eslint-plugin-rulesdir');
 rulesDirPlugin.RULES_DIR = 'eslint-rules';
 
 module.exports = {
+  parser: 'babel-eslint',
   extends: 'fbjs-opensource',
-  plugins: ['import', 'prettier', 'rulesdir'],
+  plugins: ['babel', 'import', 'prettier', 'rulesdir'],
   rules: {
+    'object-curly-spacing': 0, // false positives with export, use babel plugin's
+    'babel/object-curly-spacing': 2, // false positives with export, use babel plugin's
     'consistent-return': 0, // handled by flow
     'import/no-duplicates': 2,
     'max-len': 0, // handled by prettier
